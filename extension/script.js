59a4da7a78187f5591997e66ab1d222833a92dae
Werner Bihl <werner@tangentsolutions.co.za>
2021-04-19 08:26:22 +0200

    Theme added. Now adding keyboard shortcuts

--- extension/script.js
+++ extension/script.js
@@ -4,6 +4,7 @@ var app = {
 
     // Jupyter Notbook code collapse/expand
     app.jupyterCollapse()
+    app.applyTheme()
   },
 
   stop: () => {
@@ -11,19 +12,23 @@ var app = {
   },
 
   jupyterCollapse: () => {
-    let addSvg = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>'
-    let subSvg = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13H5v-2h14v2z"/></svg>'
 
     document.querySelectorAll('.prompt.input_prompt bdi').forEach(element => {
       var collapseButton = document.createElement('span')
-      collapseButton.innerHTML = subSvg
       collapseButton.classList.add('toggle-button')
 
       collapseButton.onclick = (event) => {
-        event.target.parentNode.classList.toggle('active')
+        event.target.classList.toggle('active')
+        event.target.parentNode.parentNode.parentNode.querySelector('.inner_cell').classList.toggle('collapsed-cell')
       }
 
       element.parentNode.insertBefore(collapseButton, element)
+    })
+  },
+
+  applyTheme: () => {
+    document.querySelectorAll('.CodeMirror').forEach(element => {
+      element.classList.add('cm-s-monokai')
     })
   }
 
